625bcd75b031d6bf4415989690e65591702f300a
yogeshwaray <yashaswi.yogeshwara@campsystems.com>
2021-09-28 17:33:58 +0530

    Inital Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
